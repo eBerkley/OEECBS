ae5b3c6ea8171559fe90791c8dbe9fd759598730
Jawad Ahsan <143557011+JawadAhsan02@users.noreply.github.com>
2025-04-15 13:03:03 -0500

    Update replanAll.cpp
    updated with bypass and so that it only modify locations past the current timestamp

--- src/replanAll.cpp
+++ src/replanAll.cpp
@@ -1,6 +1,8 @@
 #include "ECBS.h"
 #include "SpaceTimeAStar.h"
 
+extern int current_timestep; // External variable that tracks the current simulation time
+
 bool ECBS::solveReplanAll(double time_limit, int _cost_lowerbound) {
     this->cost_lowerbound = _cost_lowerbound;
     this->inadmissible_cost_lowerbound = 0;
@@ -34,30 +36,89 @@ bool ECBS::solveReplanAll(double time_limit, int _cost_lowerbound) {
         num_HL_expanded++;
         curr->time_expanded = num_HL_expanded;
 
-        ECBSNode* child[2] = { new ECBSNode(), new ECBSNode() };
-        curr->conflict = chooseConflict(*curr);
-        addConstraints(curr, child[0], child[1]);
+        if (bypass && curr->chosen_from != "cleanup") {
+            bool foundBypass = true;
+            while (foundBypass) {
+                if (terminate(curr)) return solution_found;
+                foundBypass = false;
+                ECBSNode* child[2] = { new ECBSNode(), new ECBSNode() };
+                curr->conflict = chooseConflict(*curr);
+                addConstraints(curr, child[0], child[1]);
 
-        if (screen > 1)
-            cout << "\tExpand " << *curr << endl << "\ton " << *(curr->conflict) << endl;
+                if (screen > 1)
+                    cout << "\tExpand " << *curr << endl << "\ton " << *(curr->conflict) << endl;
 
-        bool solved[2] = { false, false };
-        vector<vector<PathEntry>*> path_copy(paths);
-        vector<int> fmin_copy(min_f_vals);
+                bool solved[2] = { false, false };
+                vector<vector<PathEntry>*> path_copy(paths);
+                vector<int> fmin_copy(min_f_vals);
+                for (int i = 0; i < 2; i++) {
+                    if (i > 0) {
+                        paths = path_copy;
+                        min_f_vals = fmin_copy;
+                    }
+                    solved[i] = generateChild(child[i], curr, current_timestep);
+                    if (!solved[i]) {
+                        delete child[i];
+                        continue;
+                    } else if (i == 1 && !solved[0])
+                        continue;
+                    else if (bypass &&
+                        child[i]->sum_of_costs <= suboptimality * cost_lowerbound &&
+                        child[i]->distance_to_go < curr->distance_to_go) {
 
-        for (int i = 0; i < 2; i++) {
-            if (i > 0) {
-                paths = path_copy;
-                min_f_vals = fmin_copy;
+                        foundBypass = true;
+                        for (const auto& path : child[i]->paths) {
+                            if ((double)path.second.first.size() - 1 > suboptimality * fmin_copy[path.first]) {
+                                foundBypass = false;
+                                break;
+                            }
+                        }
+                        if (foundBypass) {
+                            adoptBypass(curr, child[i], fmin_copy);
+                            if (screen > 1) cout << "\tUpdate " << *curr << endl;
+                            break;
+                        }
+                    }
+                }
+
+                if (foundBypass) {
+                    for (auto& i : child) delete i;
+                    classifyConflicts(*curr);
+                } else {
+                    for (int i = 0; i < 2; i++) {
+                        if (solved[i]) {
+                            pushNode(child[i]);
+                            curr->children.push_back(child[i]);
+                            if (screen > 1) cout << "\t\tGenerate " << *child[i] << endl;
+                        }
+                    }
+                }
             }
-            solved[i] = generateChild(child[i], curr);
-            if (!solved[i]) {
-                delete child[i];
-                continue;
+        } else {
+            ECBSNode* child[2] = { new ECBSNode(), new ECBSNode() };
+            curr->conflict = chooseConflict(*curr);
+            addConstraints(curr, child[0], child[1]);
+
+            if (screen > 1)
+                cout << "\tExpand " << *curr << endl << "\ton " << *(curr->conflict) << endl;
+
+            bool solved[2] = { false, false };
+            vector<vector<PathEntry>*> path_copy(paths);
+            vector<int> fmin_copy(min_f_vals);
+            for (int i = 0; i < 2; i++) {
+                if (i > 0) {
+                    paths = path_copy;
+                    min_f_vals = fmin_copy;
+                }
+                solved[i] = generateChild(child[i], curr, current_timestep);
+                if (!solved[i]) {
+                    delete child[i];
+                    continue;
+                }
+                pushNode(child[i]);
+                curr->children.push_back(child[i]);
+                if (screen > 1) cout << "\t\tGenerate " << *child[i] << endl;
             }
-            pushNode(child[i]);
-            curr->children.push_back(child[i]);
-            if (screen > 1) cout << "\t\tGenerate " << *child[i] << endl;
         }
 
         switch (curr->conflict->type) {
